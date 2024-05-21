9f4ff9ea50461bad4ccbfffa4a9831ce57a6eb91
Tegibar1983 <tbtf1991@gmail.com>
2024-05-21 09:55:41 +0000

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -17,24 +17,24 @@ def validUTF8(data):
         mask_byte = 1 << 7
 
         if number_bytes == 0:
-            
+
             while mask_byte & i:
                 number_bytes += 1
                 mask_byte = mask_byte >> 1
 
             if number_bytes == 0:
-                    continue
+                continue
 
             if number_bytes == 1 or number_bytes > 4:
                 return False
 
-            else:
-                if not (i & mask_1 and not (i & mask_2));
-                return False
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
 
-            number_bytes -= 1
+        number_bytes -= 1
 
-            if number_bytes == 0:
-                return True
+    if number_bytes == 0:
+        return True
 
-            return False
+    return False
